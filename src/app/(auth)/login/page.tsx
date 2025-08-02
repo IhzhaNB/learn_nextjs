ddb337ad880d5f8d8ab681245fe788a17caeea37
unknown <ihzhanauvalb@gmail.com>
2025-08-02 14:20:20 +0700

    metadata

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: { absolute: "Login to your page" }, // Ini akan mengabaikan template dari layout induk
+};
+
 export default function Login() {
   return <div>Login</div>;
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog", // Ini akan menggantikan %s di template
+};
+
 export default function Blog() {
   return <h1>My Blog</h1>;
 }
