780cb64c0b46c9c3425dbc5355f794b0f445e316
unknown <ihzhanauvalb@gmail.com>
2025-08-03 10:51:56 +0700

    navigating programmatically: router.replace()

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,27 @@
-import { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: { absolute: "Login to your page" }, // Ini akan mengabaikan template dari layout induk
-};
+// import { Metadata } from "next";
+import { useRouter } from "next/navigation";
+
+// export const metadata: Metadata = {
+//   title: { absolute: "Login to your page" }, // Ini akan mengabaikan template dari layout induk
+// };
 
 export default function Login() {
-  return <div>Login</div>;
+  const router = useRouter();
+
+  const handleLogin = () => {
+    const masuk = confirm("Apakah anda ingin masuk?");
+
+    if (masuk) {
+      router.replace("/dashboard");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button onClick={handleLogin}>Masuk</button>
+    </div>
+  );
 }
