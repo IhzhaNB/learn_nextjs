c458e04000934b1e9240e796d5d1cc2ee21ac4aa
unknown <ihzhanauvalb@gmail.com>
2025-08-04 09:55:21 +0700

    Parallel Routes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
-export default function About() {
+export default async function About() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("internal delay");
+    }, 2000);
+  });
+
   return <h1>About Me</h1>;
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,12 @@ export const metadata: Metadata = {
   title: "Blog", // Ini akan menggantikan %s di template
 };
 
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("internal delay");
+    }, 2000);
+  });
+
   return <h1>My Blog</h1>;
 }
